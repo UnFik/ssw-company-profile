e0da31b1d8a1a502d27125b773dfa60c5fc2859a
UnFik <fikriilhamarifin_1313621027@mhs.unj.ac.id>
2023-09-13 11:58:16 +0700

    fix: remove unuse component

--- app/(routes)/tes/page.tsx
+++ app/(routes)/tes/page.tsx
@@ -15,7 +15,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { toast } from "@/components/ui/use-toast";
 
 const FormSchema = z.object({
   username: z.string().min(2, {
@@ -30,14 +29,7 @@ const Page = () => {
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     console.log(data);
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
+
   }
 
   return (
